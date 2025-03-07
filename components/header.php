446c5c321d807ac163e8742aac33dfdcb4842dde
Iago Pierre <pierrestorniolo@gmail.com>
2025-03-06 21:48:34 -0300

    Initial commit

---
+++ components/header.php
@@ -0,0 +1,20 @@
+<!-- HEADER -->
+<header class=" bg-stone-900">
+
+    <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4">
+
+        <div class="font-bold text-xl tracking-wide">Movie Wise</div>
+
+        <ul class="flex space-x-4 font-medium">
+            <li><a href="/" class="text-rose-600">Explorar</a></li>
+            <li><a href="/meus-filmes" class="hover:underline">Meus Filmes</a></li>
+        </ul>
+
+        <ul>
+            <li><a href="/login">Fazer login</a></li>
+        </ul>
+
+    </nav>
+
+</header>
+<!-- END HEADER -->
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MovieWise</title>
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+</head>
+
+<body class="bg-stone-950 text-stone-200">
+
+    <?php include 'components/header.php'; ?>
+
+    <main>
+        <!-- lista de filmes -->
+    </main>
+
+</body>
+
+</html>
